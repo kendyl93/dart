b7947dbdd1c9523b8111bbf92e08d3fcfbb7b36d
Pawel Stanecki <p.stanecki93@gmail.com>
2020-03-26 09:37:19 +0100

    Connect players inputs to redux

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-import React, { useState } from "react"
+import React from "react"
+
+import CreateGameView from "./Game/CreateGameView"
 
 import "./App.css"
 
 const App = () => {
-  const [playerInputs, setPlayerInput] = useState([{ value: "" }])
-
-  const handleChangePlayerInput = (idx, event) => {
-    const values = [...playerInputs]
-    values[idx].value = event.target.value
-
-    setPlayerInput(values)
-  }
-
-  const addPlayerInput = () => {
-    const values = [...playerInputs]
-    values.push({ value: null })
-    setPlayerInput(values)
-  }
-
-  const removePlayerInput = idx => {
-    const values = [...playerInputs]
-    values.splice(idx, 1)
-
-    setPlayerInput(values)
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Create game</h1>
       </header>
-      <h3>Type players names:</h3>
-      <form>
-        {playerInputs.map(({ value }, idx) => {
-          const playersCount = idx + 1
-
-          return (
-            <div key={`player-${playersCount}`}>
-              {`${playersCount}`}.
-              <input
-                value={value}
-                type="text"
-                onChange={event => handleChangePlayerInput(idx, event)}
-              />
-              <button type="button" onClick={() => removePlayerInput(idx)}>
-                X
-              </button>
-            </div>
-          )
-        })}
-        <button type="button" onClick={addPlayerInput}>
-          +
-        </button>
-      </form>
+      <CreateGameView />
     </div>
   )
 }
--- src/Game/CreateGameView.js
+++ src/Game/CreateGameView.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import uniqid from "uniqid"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
@@ -7,11 +7,69 @@ import { createGame } from "../stores/game/gameEffect"
 import { getPlayers } from "../stores/game/gameReducer"
 
 const CreateGameView = ({ actions: { createGame } }) => {
-  const handleStartGame = event => {
-    createGame({ [uniqid()]: { name: "Janek" }, [uniqid()]: { name: "Pawel" } }) // here push the players
+  const [playerInputs, setPlayerInput] = useState([{ value: "", id: uniqid() }])
+
+  const handleChangePlayerInput = (index, event) => {
+    const values = [...playerInputs]
+    values[index].value = event.target.value
+
+    setPlayerInput(values)
   }
 
-  return <button onClick={handleStartGame}>START</button>
+  const addPlayerInput = () => {
+    const values = [...playerInputs]
+    values.push({ value: null, id: uniqid() })
+    setPlayerInput(values)
+  }
+
+  const removePlayerInput = index => {
+    const values = [...playerInputs]
+    values.splice(index, 1)
+
+    setPlayerInput(values)
+  }
+
+  const handleStartGame = players => {
+    createGame(players)
+  }
+
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Create game</h1>
+      </header>
+      <h3>Type players names:</h3>
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          event.stopPropagation()
+          handleStartGame(playerInputs)
+        }}
+      >
+        {playerInputs.map(({ value, id }, index) => {
+          const playersCount = index + 1
+
+          return (
+            <div key={`player-${id}`}>
+              {`${playersCount}`}.
+              <input
+                value={value}
+                type="text"
+                onChange={event => handleChangePlayerInput(index, event)}
+              />
+              <button type="button" onClick={() => removePlayerInput(index)}>
+                X
+              </button>
+            </div>
+          )
+        })}
+        <button type="button" onClick={addPlayerInput}>
+          +
+        </button>
+        <input type="submit" value="START" />
+      </form>
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
